c76c8a5b947a3c03853d64dd3d6b46f3e5010e1f
DucCuong <lamduccuong123@gmail.com>
2022-11-13 14:20:39 +0700

    first commit

---
+++ demoRedis/src/main/java/com/example/demoRedis/dto/Employee.java
@@ -0,0 +1,33 @@
+package com.example.demoRedis.dto;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements Serializable {
+	private static final long serialVersionUID = 4112198546343192807L;
+
+	private Long id;
+	@JsonProperty("employee_id")
+	private String employeeId;
+	@JsonProperty("first_name")
+	private String firstName;
+	@JsonProperty("last_name")
+	private String lastName;
+	private String gender;
+	@JsonProperty("work_dept")
+	private String workDept;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
---
+++ demoRedis/src/main/java/com/example/demoRedis/dto/EmployeeIdReq.java
@@ -0,0 +1,26 @@
+package com.example.demoRedis.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeIdReq implements Serializable {
+
+	private static final long serialVersionUID = -8571454453894445935L;
+	@JsonProperty("employees")
+	private List<Long> id;
+	public List<Long> getId() {
+		return id;
+	}
+	public void setId(List<Long> id) {
+		this.id = id;
+	}
+}
